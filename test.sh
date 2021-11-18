6e2656a2654fda04c58eeb415b6bf4e07aabe781
ecemlago <ecem.lago@radboudumc.nl>
2021-11-18 14:17:22 +0100

    changed python command to python3

--- test.sh
+++ test.sh
@@ -12,12 +12,12 @@ docker run --rm \
         -v noduledetection-output:/output/ \
         noduledetector
 
-docker run --rm -v noduledetection-output:/output/ python:3.7-slim cat /output/nodules.json | python -m json.tool
+docker run --rm -v noduledetection-output:/output/ python:3.7-slim cat /output/nodules.json | python3 -m json.tool
 
 docker run --rm \
         -v noduledetection-output:/output/ \
         -v $SCRIPTPATH/test/:/input/ \
-        python:3.7-slim python -c "import json, sys; f1 = json.load(open('/output/nodules.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
+        python:3.7-slim python3 -c "import json, sys; f1 = json.load(open('/output/nodules.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
 
 if [ $? -eq 0 ]; then
     echo "Tests successfully passed..."
