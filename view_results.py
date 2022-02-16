f162d67a640af87fc7ccbfb2246030217df7483e
nkitner <nicolekitner@gmail.com>
2022-02-16 10:19:37 -0500

    blue for best prediction in view_results

--- view_results.py
+++ view_results.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw
 import os
 from PIL import ImageFont
 
-path_name = "/Users/nikki/Desktop/node21_detection_baseline/input/"
+path_name = "/Volumes/ExternalHardDrive/node21_detection_baseline/Untitled/input/"
 images = []
 for filename in os.listdir(path_name):
     full_path = path_name + filename
@@ -19,26 +19,35 @@ for filename in os.listdir(path_name):
     images.append(img)
 
 # plt.imshow(image_arr)
-with open('/Users/nikki/Desktop/node21_detection_baseline/output/nodules.json', 'r') as f:
+with open('/Volumes/ExternalHardDrive/node21_detection_baseline/Untitled/output/nodules.json', 'r') as f:
     bounding_boxes = json.load(f)
 
 points = bounding_boxes['boxes']
 
-for i,img in enumerate(images):
+for k,img in enumerate(images):
     # create rectangle image
-    point = points[i]
+    point = points[k]
     for i, point in enumerate(points):
         probability = point['probability']
         img1 = ImageDraw.Draw(img)
-
-        corner_pts = point['corners']
-        text_pos = (int(corner_pts[0]), int(corner_pts[1] - 30))
-        font = ImageFont.truetype("/Library/Fonts/Arial.ttf", size=24)
-        img1.text(
-          text_pos,  # Coordinates
-          str(probability),  # Text
-          (255, 0, 0),  # Color
-          font=font
-        )
-        img1.rectangle(corner_pts, outline="red")
+        if probability > 0.2:
+            corner_pts = point['corners']
+            text_pos = (int(corner_pts[0]), int(corner_pts[1] - 30))
+            font = ImageFont.truetype("/Library/Fonts/Arial.ttf", size=24)
+            if i == 0:
+                img1.rectangle(corner_pts, outline="blue")
+                img1.text(
+                    text_pos,  # Coordinates
+                    str(probability),  # Text
+                    (0, 0, 255),  # Color
+                    font=font
+                )
+            else:
+                img1.rectangle(corner_pts, outline="red")
+                img1.text(
+                    text_pos,  # Coordinates
+                    str(probability),  # Text
+                    (255, 0, 0),  # Color
+                    font=font
+                )
     img.show()
